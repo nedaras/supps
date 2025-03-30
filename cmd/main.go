b4d4289c84cd19f3626545f2dc7f28abc98a62d4
Nedas <nedas.pranskunas@gmail.com>
2025-03-30 19:39:09 +0300

    Added empty products page.

--- cmd/main.go
+++ cmd/main.go
@@ -9,129 +9,159 @@ import (
 )
 
 type State = struct {
-  Idx   int
-  Idxs  int
-  Flags uint8
+	Idx   uint8
+	Idxs  uint8
+	Flags uint8
 }
 
 var state State = State{
-  Idx: 0,
-  Idxs: 2,
-  Flags: 0,
+	Idx:   0,
+	Idxs:  2,
+	Flags: 0,
 }
 
 const (
-    MrBiceps  int = 0
-    MyProtein int = 1
-    Protein  int = 2
-    Creatine  int = 3
+	MrBiceps  uint8 = 0
+	MyProtein uint8 = 1
+	Protein   uint8 = 2
+	Creatine  uint8 = 3
 )
+
 func main() {
-  s, err := tcell.NewScreen()
-  if err != nil {
-    panic(err)
-  }
-  defer s.Fini()
+	s, err := tcell.NewScreen()
+	if err != nil {
+		panic(err)
+	}
+	defer s.Fini()
 
-  if err := s.Init(); err != nil {
-    panic(err)
-  }
+	if err := s.Init(); err != nil {
+		panic(err)
+	}
 
-  render(s);
-  s.Show()
+	render(s)
+	s.Show()
 
-  for {
-    switch ev := s.PollEvent().(type) {
-    case *tcell.EventKey:
-      if (ev.Key() == tcell.KeyUp) || (ev.Key() == tcell.KeyRune && ev.Rune() == 'k') {
-        state.Idx = (state.Idx + state.Idxs - 1) % state.Idxs
-      }
+	for {
+		switch ev := s.PollEvent().(type) {
+		case *tcell.EventKey:
+			flag := state.Flags&0b10000000 != 0
 
-      if (ev.Key() == tcell.KeyDown) || (ev.Key() == tcell.KeyRune && ev.Rune() == 'j') {
-        state.Idx = (state.Idx + 1) % state.Idxs
-      }
+			if ev.Key() == tcell.KeyRune && ev.Rune() == 'q' {
+				return
+			}
 
-      if ev.Key() == tcell.KeyEnter {
-        state.Flags ^= (1 << state.Idx)
-      }
+			if flag {
+			} else {
+				if (ev.Key() == tcell.KeyUp) || (ev.Key() == tcell.KeyRune && ev.Rune() == 'k') {
+					state.Idx = (state.Idx + state.Idxs - 1) % state.Idxs
+				}
 
-      if state.Flags & 0x3 != 0 {
-        state.Idxs = 4
-      } else {
-        state.Flags = state.Flags & 0x3
-        state.Idxs = 2
-      }
+				if (ev.Key() == tcell.KeyDown) || (ev.Key() == tcell.KeyRune && ev.Rune() == 'j') {
+					state.Idx = (state.Idx + 1) % state.Idxs
+				}
 
-      if ev.Key() == tcell.KeyRune && ev.Rune() == 'q' {
-        return
-      }
-    }
-    s.Clear()
-    render(s);
-    s.Show()
-  }
+				if ev.Key() == tcell.KeyEnter {
+					state.Flags ^= (1 << state.Idx)
+				}
+
+				if state.Flags&0x3 != 0 {
+					state.Idxs = 4
+				} else {
+					state.Flags = state.Flags & 0b10000011
+					state.Idxs = 2
+				}
+			}
+
+			if ev.Key() == tcell.KeyRune && ev.Rune() == 's' {
+				state.Flags = state.Flags & 0b01111111
+			}
+
+			if ev.Key() == tcell.KeyRune && ev.Rune() == 'p' {
+				state.Flags = state.Flags | 0b10000000
+			}
+		}
+		s.Clear()
+		render(s)
+		s.Show()
+	}
 }
 
 func render(s tcell.Screen) {
-  white := tcell.StyleDefault.Foreground(tcell.ColorWhite)
-  gray := tcell.StyleDefault.Foreground(tcell.ColorGray)
+	white := tcell.StyleDefault.Foreground(tcell.ColorWhite)
+	gray := tcell.StyleDefault.Foreground(tcell.ColorGray)
+	flag := state.Flags&0b10000000 != 0
 
-  text(s, 0, 0, "┌────────────────┬───────────────────┬────────────────────┐", gray)
-  text(s, 0, 1, "│                │                   │                    │", gray)
-  text(s, 0, 2, "└────────────────┴───────────────────┴────────────────────┘", gray)
+	text(s, 0, 0, "┌────────────────┬───────────────────┬────────────────────┐", gray)
+	text(s, 0, 1, "│                │                   │                    │", gray)
+	text(s, 0, 2, "└────────────────┴───────────────────┴────────────────────┘", gray)
 
-  text(s, 5, 1, "supps.go", white.Bold(true))
+	text(s, 5, 1, "supps.go", white.Bold(true))
+	text(s, 22, 1, "s", white)
 
-  text(s, 22, 1, "s selection", white)
+	if flag {
+		text(s, 24, 1, "selection", gray)
+	} else {
+		text(s, 24, 1, "selection", white)
+	}
 
-  text(s, 42, 1, "p", white)
-  text(s, 44, 1, "products", gray)
-  text(s, 53, 1, strconv.Itoa(bits.OnesCount8(state.Flags >> 2)), white)
+	text(s, 42, 1, "p", white)
 
-  text(s, 0, 16, "───────────────────────────────────────────────────────────", gray)
+	if flag {
+		text(s, 44, 1, "products", white)
+	} else {
+		text(s, 44, 1, "products", gray)
+	}
 
-  mrbiceps := gray
-  myprotein := gray
+	text(s, 53, 1, strconv.Itoa(bits.OnesCount8(state.Flags&0b01111100)), white)
 
-  if state.Idx == MrBiceps {
-    mrbiceps = white.Background(tcell.ColorDarkOrange)
-  }
+	text(s, 0, 16, "───────────────────────────────────────────────────────────", gray)
 
-  if state.Idx == MyProtein {
-    myprotein = white.Background(tcell.ColorBlue)
-  }
+	if flag {
 
-  prefix := func(idx int) string {
-    if state.Flags & (1 << idx) != 0 {
-      return "◉"
-    }
-    return "○"
-  }
+	} else {
+		mrbiceps := gray
+		myprotein := gray
 
-	text(s, 2, 4, "~ select shops ~", white)
-	text(s, 1, 5, " " + prefix(MrBiceps) + " MrBiceps          ", mrbiceps)
-	text(s, 1, 6, " " + prefix(MyProtein) + " MyProtein         ", myprotein)
+		if state.Idx == MrBiceps {
+			mrbiceps = white.Background(tcell.ColorDarkOrange)
+		}
 
-  if state.Flags & 0x3 != 0 {
-    proteine := gray
-    creteine := gray
+		if state.Idx == MyProtein {
+			myprotein = white.Background(tcell.ColorBlue)
+		}
 
-    if state.Idx == Protein {
-      proteine = white.Background(tcell.ColorRed)
-    }
+		prefix := func(idx uint8) string {
+			if state.Flags&(1<<idx) != 0 {
+				return "◉"
+			}
+			return "○"
+		}
 
-    if state.Idx == Creatine {
-      creteine = white.Background(tcell.ColorBlack)
-    }
+		text(s, 2, 4, "~ select shops ~", white)
+		text(s, 1, 5, " "+prefix(MrBiceps)+" MrBiceps          ", mrbiceps)
+		text(s, 1, 6, " "+prefix(MyProtein)+" MyProtein         ", myprotein)
 
-    text(s, 2, 8, "~ select products ~", white)
-    text(s, 1, 9, " " + prefix(Protein) + " protein           ", proteine)
-    text(s, 1, 10, " " + prefix(Creatine) + " creatine          ", creteine)
-  }
+		if state.Flags&0x3 != 0 {
+			proteine := gray
+			creteine := gray
+
+			if state.Idx == Protein {
+				proteine = white.Background(tcell.ColorRed)
+			}
+
+			if state.Idx == Creatine {
+				creteine = white.Background(tcell.ColorBlack)
+			}
+
+			text(s, 2, 8, "~ select products ~", white)
+			text(s, 1, 9, " "+prefix(Protein)+" protein           ", proteine)
+			text(s, 1, 10, " "+prefix(Creatine)+" creatine          ", creteine)
+		}
+	}
 }
 
 func text(s tcell.Screen, x int, y int, str string, style tcell.Style) {
-  for _, c := range str {
+	for _, c := range str {
 		var comb []rune
 		w := runewidth.RuneWidth(c)
 		if w == 0 {
@@ -141,5 +171,5 @@ func text(s tcell.Screen, x int, y int, str string, style tcell.Style) {
 		}
 		s.SetContent(x, y, c, comb, style)
 		x += w
-  }
+	}
 }
